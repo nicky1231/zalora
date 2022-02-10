3d30df4c7ef52011407a3bd9da5585b64b8d7d7e
PUTRINICKY0 <98703419+PUTRINICKY0@users.noreply.github.com>
2022-02-10 19:27:58 +0700

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    // use Authenticatable, Authorizable;
+    // use HasFactory;
+
+    protected $fillable = [
+
+         'email','name','password','address','date','gender',
+    ];
+
+
+    public $timestamps = true;
+}
